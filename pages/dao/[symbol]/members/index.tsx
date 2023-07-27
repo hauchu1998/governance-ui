a05b157cff5f00546410fdb67a6cca2d77513cf8
Hau Chu <hchoward1998@gmail.com>
2023-07-26 19:02:35 -0500

    init Member page for nft-voter plugin

--- pages/dao/[symbol]/members/index.tsx
+++ pages/dao/[symbol]/members/index.tsx
@@ -1,10 +1,14 @@
+import { NFT_PLUGINS_PKS } from '@constants/plugins'
 import Members from './Members'
 import { useRealmConfigQuery } from '@hooks/queries/realmConfig'
 const MembersPage = () => {
   const config = useRealmConfigQuery().data?.result
   return (
     <div>
-      {!config?.account.communityTokenConfig.voterWeightAddin ? (
+      {!config?.account.communityTokenConfig.voterWeightAddin ||
+      NFT_PLUGINS_PKS.includes(
+        config?.account.communityTokenConfig.voterWeightAddin.toBase58()
+      ) ? (
         <Members />
       ) : null}
     </div>
